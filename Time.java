e74f27783213a908efa9a753332a343e2fc3cf02
khangv9342 <kmvo100@student.hbuhsd.edu>
2025-01-06 10:14:39 -0800

    :3

--- Time.java
+++ Time.java
@@ -1,21 +1,33 @@
 public class Time
 {
   // TODO: Part A - put your attributes here
-
+  private int hours;
+  private int minutes;
+  private int seconds; 
 
   // TODO: Part B - create your non-default constructor
   /**
   * Precondition: The number of hours is between 0 and 23 inclusive.
   * The number of minutes and seconds is between 0 and 59 inclusive.
   */
-
+ public Time(int hours, int minutes, int seconds)
+ {
+  this.hours = iHours;
+  this.minutes = iMinutes;
+  this.seconds = iSeconds;
+ }
 
   // TODO: Part C - create your default constructor
+  public Time();
+  {
+    this(0, 0, 0);  
+  }
 
 
   // TODO: Part D - complete the documentation for the pad method
   /**
-  * Description: 
+  * Description: Returns the input value as a 2-digit format. A number like 5 becomes "05" 
+  * Anything already with 2 digits does not get another 0.x
   * Precondition: The value is greater than 0.
   */
   private String pad(int value)
@@ -31,7 +43,7 @@ public class Time
   // TODO: Part E - complete the toString method; use the pad method as part of your solution
   public String toString()
   {
-    return null;
+    return pad(hours) + ":" + pad(minutes) + ":" + pad(seconds)
   }
 
   // TODO: Part F - write the tick method
@@ -39,7 +51,7 @@ public class Time
   {
 
   }
-
+x
   // TODO: Part G - write the add method
   /**
   * Precondition: The variable offset is non-null
